c046fe13b54bfe879ac1be7f3824e7a5c19d7606
Diego Frigerio <diegonf84@gmail.com>
2026-06-20 19:18:17 -0300

    chore(frontend): add ResizeObserver polyfill for nivo tests

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/vitest';
+
+// ResizeObserver polyfill for nivo responsive components in jsdom
+globalThis.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
